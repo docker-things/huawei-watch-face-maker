a12607e793690b0b8f0f1909b59bfef83fc517ce
Gabriel Ionescu <blchinezu@users.noreply.github.com>
2020-01-20 12:59:41 +0200

    Updated to 073b

--- config.sh
+++ config.sh
@@ -20,7 +20,7 @@ APP_PARAM=""
 
 # BUILD ARGS
 BUILD_ARGS=(
-    --build-arg APP_ARCHIVE_URL="https://doc-0c-ak-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/lfd9scltn9pvhv4t0m0j94e8rv02i3dk/1579082400000/11661470670177043854/*/1d0pGtG6mKITkbMOxaBXdSJnLAmNiIR0l?e=download"
+    --build-arg APP_ARCHIVE_URL="https://doc-08-ak-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/t1tg7ds4k1rvjv5ph7f61pljeekp2jrc/1579514400000/11661470670177043854/*/1283OpIQGbxc0ppLQZx9oCQbrNKYyXxEO?e=download"
     )
 
 # LAUNCH ARGS
